bc66be1c2ad69d1ba6ee13b8b3973a7901b3b33d
marcus <lastenaufzug@gmx.de>
2015-06-02 19:40:19 +0200

    Added picamera interface for rotating video

--- rotation_display/rotating_video.py
+++ rotation_display/rotating_video.py
@@ -4,21 +4,18 @@ Created on 18.05.2015
 @author: kolbe
 '''
 import cv2
+import numpy as np
 import mma7455
-
-def current_rotation():
-    # ToDo: read data from accellerometer :)
-    rotation = 0.0
-    while True:
-        yield rotation
-        rotation += 0.1
-        rotation %= 360
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import io
 
 if __name__ == '__main__':
     
     # open first video device
-    cap = cv2.VideoCapture(0)
-    ret, frame = cap.read()
+    #cap = cv2.VideoCapture(0)
+    #ret, frame = cap.read()
+    camera = PiCamera()
     
     # show fullscreen image
     cv2.namedWindow("Image", cv2.WINDOW_OPENGL)
@@ -27,6 +24,22 @@ if __name__ == '__main__':
     # open accelerometer
     mma = mma7455.MMA7455()
     # enter capture loop
+    stream = io.BytesIO()
+    for f in camera.capture_continuous(stream, format="bgr", use_video_port=True):
+        stream.seek(0)
+        data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+        res = camera.resolution
+        data = data.reshape((res[1], res[0], 3))
+        
+        # show image, abort when ESC key pressed
+        cv2.imshow("Image", data)
+        k = cv2.waitKey(1)
+        if k == 27: #ESC
+            break
+        
+        
+        
+    '''
     while ret:
         # calculate rotation matrix
         mat = cv2.getRotationMatrix2D((frame.shape[1]/2, frame.shape[0]/2), mma.getValueY(), 1.0)
@@ -43,3 +56,4 @@ if __name__ == '__main__':
         ret, frame = cap.read()
         
     cap.release()
+    '''
