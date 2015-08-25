6a12ce1f12210070d514fbf0f3135556b6721829
marcus <lastenaufzug@gmx.de>
2015-08-25 08:31:08 +0200

    mjpegview.py retry when source not avaiable, added text

--- mjpeg/mjpegview.py
+++ mjpeg/mjpegview.py
@@ -8,6 +8,7 @@ from threading import Thread
 from optparse import OptionParser
 import cv2
 import numpy
+import time
 #import gtk
 
 
@@ -15,24 +16,52 @@ image_buffer = None
 image_size = (800, 600)
 run = True
 
-
-def update_image(addr, nr):
+def draw_text(text, frame):
+    t_size = cv2.getTextSize(text, cv2.FONT_HERSHEY_COMPLEX, 0.7, 1)
+    img_size = frame.shape[:2]
+    pos = (img_size[1]/2 - t_size[0][0] /2, img_size[0] - t_size[0][1] / 2)
+    
+    cv2.rectangle(frame, (pos[0], pos[1]-t_size[0][1]), (pos[0]+t_size[0][0], pos[1]+2), (0, 0, 0), -1)
+    cv2.putText(frame, text, pos, cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 255))
+    
+def update_buffer(frame, row, col):
+    image_buffer[row*image_size[1]:row*image_size[1]+image_size[1],
+                 col*image_size[0]:col*image_size[1]+image_size[0]] = frame
+def update_image(addr, nr, show_text=False):
     global image_buffer
     global image_size
-    
-    cam = cv2.VideoCapture(addr)
-    ret, frame = cam.read()
-    
+        
     row = nr / 2
     col = nr % 2
+    
+    if show_text:
+        frame = numpy.ones((image_size[1], image_size[0], 3), numpy.uint8)
+        draw_text("open..."+addr, frame)
+        update_buffer(frame, row, col)
+    
+    ret = False
+    while not ret and run:
+        print "[%d] open video source %s" % (nr, addr)
+        cam = cv2.VideoCapture(addr)
+        ret, frame = cam.read()
+        if not ret:
+            print "[%d] cant't get frame from %s...retry in 2s" % (nr, addr)
+            time.sleep(2)
+    print "[%d] opened %s" % (nr, addr)
+
     while ret and run:
         frame = cv2.resize(frame, image_size)
         print "[%d] read frame.." % nr
         #print image_size
         #frame = numpy.ones(image_size, numpy.uint8) * 255
-        image_buffer[row*image_size[1]:row*image_size[1]+image_size[1],
-                     col*image_size[0]:col*image_size[1]+image_size[0]] = frame
+        if show_text:
+            draw_text(addr, frame)
+            
+        update_buffer(frame, row, col)
+
         ret, frame = cam.read()
+    
+    print "[%d] shutdown... %s" % (nr, addr)
 
 if __name__ == '__main__':
     parser = OptionParser()
@@ -40,7 +69,8 @@ if __name__ == '__main__':
                   help="Look for streams at address", default="")
     parser.add_option("-f", "--fullscreen", dest="fullscreen",
                   help="Fullscreen", action="store_true")
-
+    parser.add_option("-t", "--text", dest="text",
+                  help="Show text (video source)", action="store_true")
     (options, args) = parser.parse_args()
     addr = options.addr.split(',')
     
@@ -53,13 +83,16 @@ if __name__ == '__main__':
     screen_width, screen_height = (1280, 1024)
     image_buffer = numpy.zeros((screen_height, screen_width, 3), dtype=numpy.uint8)
     rows = max(len(addr) / 2, 1)
-    image_size = (image_buffer.shape[1]/2, image_buffer.shape[0]/rows)
+    if len(addr) > 1:
+        image_size = (image_buffer.shape[1]/2, image_buffer.shape[0]/rows)
+    else:
+        image_size = (image_buffer.shape[1], image_buffer.shape[0])
     
     print rows, image_size
     threads = []
     i=0
     for a in addr:
-        t = Thread(target=update_image, args=(a, i))
+        t = Thread(target=update_image, args=(a, i, options.text))
         t.start()
         threads.append(t)
         i+=1
